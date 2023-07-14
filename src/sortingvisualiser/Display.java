9af8f6f96767e98699dfb989b9834eaec0961ed0
Mark <Mark@DESKTOP-BP3KN4S>
2023-07-14 01:29:29 +0100

    Messy display scaling working

--- src/sortingvisualiser/Display.java
+++ src/sortingvisualiser/Display.java
@@ -4,12 +4,11 @@
  */
 package sortingvisualiser;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
-import javax.swing.JFrame;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import javax.swing.JPanel;
-import javax.swing.WindowConstants;
 
 /**
  *
@@ -18,49 +17,67 @@ import javax.swing.WindowConstants;
 public class Display extends JPanel{
     private Color backgroundColour, barColour;
     int[] displayData;
-    int heightScale;
+    double heightScale;
+    double widthScale;
+    int width;
+    int height;
     
     public Display() {
         backgroundColour = Color.BLACK;
         barColour = Color.WHITE;
         heightScale = 1;
+        widthScale = 1;
+        width = 1280;
+        height = 720;
     }
     
     @Override
     public void paintComponent(Graphics g) {
+        // Scale the image
+        updateScaling();
+        super.paintComponent(g);
+        Graphics2D g2D = (Graphics2D) g.create();
+        g2D.scale(widthScale, heightScale);
+        g2D.translate(0, (- height * heightScale) + height);
+        System.out.println(height);
         // Draw the background
-        g.setColor(backgroundColour);
-        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        g2D.setColor(backgroundColour);
+        g2D.fillRect(0, this.getHeight() - height, width, height);
         if (displayData != null) {
-            paintBars(g);
+            paintBars(g2D);
         }
     }
     
-    public void paintBars(Graphics g) {
+    public void paintBars(Graphics2D g) {
         g.setColor(barColour);
-        int barWidth = (int) (getWidth() / displayData.length);
         //System.out.println("Width: " + getWidth());
         //System.out.println("barWidth: " + barWidth);
-        for(int i = 0; i < displayData.length; i++) {
-            int barHeight = displayData[i] * heightScale;
-            int x = i * barWidth;
+        for(int x = 0; x < displayData.length; x++) {
+            int barHeight =  displayData[x];
             int y = getHeight() - barHeight;
-            g.fillRect(x, y, barWidth, barHeight);
-        }
+            g.fillRect(x, y, 1, barHeight);
+        }       
     }
     
     public void update (int[] intArray) {
         displayData = intArray;
-        
+        updateScaling();
+        repaint();
+    }
+    
+    public void updateScaling() {
         int largest = displayData[0];
-        for(int i : intArray) 
+        for(int i : displayData) 
         {
             largest = i > largest ? i : largest;
         };
-        heightScale = (int) (getHeight() / largest);
-
-        repaint();
-    } 
+        width = displayData.length;
+        height = largest;
+        heightScale = (double) getHeight() / largest;
+        widthScale = (double) getWidth() / displayData.length;
+        //System.out.println(heightScale);
+        
+    }
 
     public void setBackgroundColour(Color backgroundColour) {
         this.backgroundColour = backgroundColour;
